010ee9d6f839b7b2948933ac53d8ec08e1fa9bf4
Steve Balistreri <steve@wekink.com>
2018-01-13 18:32:18 -0600

    basic layout, can search and add entries to day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    MomentModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,67 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as Fuse from 'fuse.js';
+import * as _ from 'lodash';
+import { caffeineData, CaffeineItem } from '../../caffeineData';
+
+interface CaffeineEntry {
+  timestamp: number;
+  item: CaffeineItem;
+}
+
+interface JournalDay {
+  day: Date;
+  entries: Array<CaffeineEntry>;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  searchResults: Array<any>;
+  todaysEntries: Array<CaffeineEntry> = [];
+  todaysTotal: number;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  getItems(event) {
+    const searchByText = event.srcElement.value;
+    const fuseOptions: Fuse.FuseOptions = {
+      shouldSort: true,
+      threshold: 0.2,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: ["name"]
+    }
+    const fuseSearch = new Fuse(caffeineData, fuseOptions);
+    const result = fuseSearch.search(searchByText);
+    this.searchResults = result.slice(0, 6);
+  }
+
+  selectItem(item: CaffeineItem) {
+    const newEntry: CaffeineEntry = {
+      timestamp: Date.now(),
+      item
+    }
+    this.todaysEntries.push(newEntry);
+    this.searchResults = [];
+    this.setTotalIntake(this.todaysEntries);
+    console.log(this.todaysTotal)
+  }
+
+  setTotalIntake(entries: Array<CaffeineEntry>) {
+    let total: number = _.map(entries, (entry) => {
+      return _.reduce(entry.item.totalCaffeine, (prev, next) => {
+        console.log(Number(prev) + Number(next));
+        return prev + next
+      })
+    })
+    console.log(;
+  }
 }
